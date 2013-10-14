d7b3647e985774b9f974b6b92a030694f621fd2d
DanDan <zyiiilo@hotmail.com>
2013-10-14 22:24:51 +0200

    GetAccount done
    Refactored getaccounts()
    Restructured code

--- Innlevering2/src/client/Client.java
+++ Innlevering2/src/client/Client.java
@@ -11,6 +11,10 @@ public class Client {
 		try(AccountMaintenance acmt = new AccountMaintenance(args[0], args[1])) {
 			
 			Map<String, Account> test = AccountMaintenance.getAccounts("accounts");
+			Account account = AccountMaintenance.getAccount("accounts", "22222222");
+			
+			System.out.println(account);
+			System.out.println(test);
 			
 			} catch (Exception e) {
 			e.printStackTrace();
--- Innlevering2/src/maintenance/AccountMaintenance.java
+++ Innlevering2/src/maintenance/AccountMaintenance.java
@@ -10,6 +10,7 @@ import java.util.*;
 import db.ConnectToDB;
 
 public class AccountMaintenance implements AutoCloseable {
+	private static final String QUERY = "SELECT * FROM ";
 	private ConnectToDB db;
 	private static Connection connection;
 
@@ -18,47 +19,61 @@ public class AccountMaintenance implements AutoCloseable {
 		connection = db.getConnection();
 	}
 
-	public static void updateAccounts(String tableName, String tableUpdate)
-			throws SQLException {
-		getAccounts(tableName);
+	public static void updateAccounts(String tableName, String tableUpdate) throws SQLException {
+		Map<String, Account> accounts = getAccounts(tableName);
 		
 	}
 
-	public static Map<String, Account> getAccounts(String tableName)
-			throws SQLException {
+	public static Map<String, Account> getAccounts(String tableName) throws SQLException {
 		try (Statement stmt = connection.createStatement()) {
 			
 			Map<String, Account> mapOfAccounts = new HashMap<String, Account>();
 			
-			String query = "Select * from " + tableName;
-
+			List<String> content = dbQuery(QUERY + tableName);
+			List<Account> newAccountObjects = makeNewAccounts(content, 3);
+			
+			for (Account account2 : newAccountObjects) {
+				mapOfAccounts.put(String.valueOf(account2.getAccountNumber()), account2);
+			}
+			
+			return mapOfAccounts;
+		}
+	}
+	
+	public static Account getAccount(String tableName, String accountNumber) throws SQLException {
+		String getAccQuery = tableName + " WHERE ACCOUNTNUMBER = " + accountNumber;
+		
+		List<String> accountData = dbQuery(QUERY + getAccQuery);
+		
+		Account account = new Account(
+				Integer.valueOf(accountData.get(0)),
+				Integer.valueOf(accountData.get(1)), 
+				Double.valueOf(accountData.get(2))
+				);
+		
+		return account;
+	}
+	
+	public static List<String> dbQuery(String query) throws SQLException {
+		try(Statement stmt = connection.createStatement()) {
+			
 			ResultSet rs = stmt.executeQuery(query);
 			ResultSetMetaData rsMetaData = rs.getMetaData();
-
-			ArrayList<String> content = new ArrayList<String>();
-
-			while (rs.next()) {
+			
+			List<String> content = new ArrayList<>();
+			
+			while(rs.next()) {
 				for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
 					content.add(rs.getObject(i).toString());
 				}
 			}
-
-			List<Account> makeNewAccounts = makeNewAccounts(content, rsMetaData.getColumnCount());
-			
-			for (Account account2 : makeNewAccounts) {
-				mapOfAccounts.put(String.valueOf(account2.getAccountNumber()),
-						account2);
-			}
-			return mapOfAccounts;
+			return content;
 		}
 	}
 
-	public static Account getAccount(String tableName, String accountNumber) {
-		return null;
-	}
 	
-	private static List<Account> makeNewAccounts(ArrayList<String> content, int numberOfColumns) {
-		ArrayList<Account> account = new ArrayList<Account>();
+	private static List<Account> makeNewAccounts(List<String> content, int numberOfColumns) {
+		List<Account> account = new ArrayList<Account>();
 
 		for (int i = 0; i < content.size(); i += numberOfColumns) {
 			account.add(new Account(
